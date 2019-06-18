0c098e3585b10b99f5949550b6d4bdad984a7597
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-18 16:09:36 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,7 +10,7 @@ def shipping_manifest
 end
 
 def retrieval
-  shipping_manifest[3]
+  shipping_manifest["oil paintings"]
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
